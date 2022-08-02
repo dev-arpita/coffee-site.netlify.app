1adfddb43befb4fdec754234cdfd72d7cd2b97ac
saibatechnoexponent <saibal@technoexponent.com>
2022-08-02 19:58:57 +0530

    This is my First commit .

---
+++ index.js
@@ -0,0 +1,9 @@
+let messageEl = document.getElementById("message-el")
+
+function signin() {
+    messageEl.textContent = "Welcome! Thank You for sign in"
+}
+
+function signup() {
+    messageEl.textContent = "Opps! We are missing you"
+}
